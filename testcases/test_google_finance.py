e8b5c7a8e58cab358e6de06b8c332c532053505c
mohamed-fazil-elastiq <mohamed.fazil@elastiq.ai>
2024-08-27 23:30:12 +0530

    added CI

--- testcases/test_google_finance.py
+++ testcases/test_google_finance.py
@@ -1,6 +1,17 @@
 from pages.google_finance_page import GoogleFinancePage
 
-def test_google_finance(driver):
+def test_google_finance_page_title(driver):
     google_finance_page = GoogleFinancePage(driver)
     google_finance_page.verify_page_title()
-    google_finance_page.compare_stock_symbols()
+
+def test_symbols_missing_in_ui(driver):
+    google_finance_page = GoogleFinancePage(driver)
+    missing_in_ui = google_finance_page.get_missing_symbols()
+    print(f"Symbols missing in UI: {missing_in_ui}")
+
+
+def test_extra_symbols_in_ui(driver):
+    google_finance_page = GoogleFinancePage(driver)
+    extra_in_ui = google_finance_page.get_extra_symbols()
+    print(f"Extra symbols in UI: {extra_in_ui}")
+
